9996f93130a3718ed886894e4ac1e03d0080f91a
Shreesh-Kulkarni <kshreesh5@gmail.com>
2023-07-01 23:46:51 +0530

    4 bit ring counter

---
+++ counters/ringc.v
@@ -0,0 +1,10 @@
+module ringc(clk,rst,out);
+input clk,rst;
+output reg[3:0] out;
+always @(posedge clk or posedge rst ) begin
+    if(rst)
+    out<=4'b1000;
+    else
+    out<={out[0],out[3],out[2],out[1]};
+end
+endmodule
