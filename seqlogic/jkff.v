467a2130903e44d912f6c2c32cdaec6a38eae789
Shreesh-Kulkarni <kshreesh5@gmail.com>
2023-10-24 01:06:09 +0530

    jkff

--- seqlogic/jkff.v
+++ seqlogic/jkff.v
@@ -1,10 +1,12 @@
-module jkff(j,k,q,qbar,clk);
-input j,k,clk;
+module jkff(j,k,q,qbar,clk,rst);
+input j,k,clk,rst;
 output reg q;
 output qbar;
 assign qbar=~q;
-always @(posedge clk ) begin
-    if(j==1'b0 && k==1'b0)
+always @(posedge clk) begin
+    if(rst)
+    q<=1'b0;
+    else if(j==1'b0 && k==1'b0)
     q<=q;
     else if(j==1'b1 && k==1'b0)
     q<=1;
--- seqlogic/jkfftb.v
+++ seqlogic/jkfftb.v
@@ -1,13 +1,19 @@
 `include "jkff.v"
 `timescale 1ns/1ns
 module jkfftb;
-reg j,k,clk;
+reg j,k,clk,rst;
 wire q,qbar;
-jkff DUT(.j(j),.k(k),.clk(clk),.q(q),.qbar(qbar));
+jkff DUT(.j(j),.k(k),.clk(clk),.q(q),.qbar(qbar),.rst(rst));
 initial begin
     clk=1'b0;
     forever
     #5 clk=~clk;
+end
+initial begin
+    rst=1'b0;
+    forever
+    #15 rst=~rst;
+
 end
 initial begin
     $dumpfile("jkfftb.vcd");
@@ -20,6 +26,6 @@ initial begin
     #25 j=1'b0;k=1'b0;
     #30 j=1'b1;k=1'b1;
     #35 j=1'b0;k=1'b0;
-    $finish;
+    #200 $finish;
 end
 endmodule
