f7606588f2ae77271a659686217b5a268d5d3cea
Shreesh-Kulkarni <kshreesh5@gmail.com>
2023-10-24 01:02:41 +0530

    .

--- comblogic/adderstrtb.v
+++ comblogic/adderstrtb.v
@@ -1,20 +1,26 @@
 `include "adderstr.v"
 `timescale 1ps/1ps
 module adderstrtb;
-reg[3:0]a,b;
+reg[2:0]a,b;
 reg cin;
-wire[3:0]sum;
+wire[2:0]sum;
 wire cout;
 adderstr DUT(a,b,cin,sum,cout);
+integer i;
 initial
 begin
     $dumpfile("adderstrtb.vcd");
     $dumpvars(0,adderstrtb);
+    a=3'd0;b=3'd0;cin=1'b0;
     $monitor($time," A=%b B=%b Cin=%b Sum=%b Cout=%b",a,b,cin,sum,cout);
-    #5 a=4'b1010;b=4'b1100;cin=1'b0;
-    #5 a=4'b0010;b=4'b1110;cin=1'b1;
-    #5 a=4'b1111;b=4'b1111;cin=1'b0;
-    #5 a=4'b1001;b=4'b1101;cin=1'b1;
-    #5 $finish;
+    forever begin
+        #5 a=a+1'b1;
+        b=b+1'b1;
+        cin=~cin;
+    end
+end
+initial begin
+    #50;
+    $finish;
 end
 endmodule
