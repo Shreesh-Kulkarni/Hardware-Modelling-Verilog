adb3d9054bc3d8174b23972bfa86f764bbfcd74a
Shreesh-Kulkarni <kshreesh5@gmail.com>
2024-01-16 18:48:40 +0530

    4bit updown with load/enable

--- counters/fbitsyncud.v
+++ counters/fbitsyncud.v
@@ -1,12 +1,19 @@
-module fbitsyncud(clk,rst,ud,count);
-input clk,rst,ud;
+module updown(clk,rst,enable,load,upordown,data_in,count);
+input clk,rst,load,enable,upordown;
+input[3:0] data_in;
 output reg[3:0]count;
 always @(posedge clk or posedge rst ) begin
     if(rst)
     count<=4'h0;
-    else if(ud)
-    count<=count+1'b1;
-    else
-    count<=count-1'b1;
+    else if(enable)begin
+        if(load)
+        count<=data_in;
+        else begin
+            if(upordown)
+            count<=count+1'b1;
+            else
+            count<=count-1'b1;
+        end
+    end
 end
 endmodule
